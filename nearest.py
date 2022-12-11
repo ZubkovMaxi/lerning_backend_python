b5a9c978788723987795b2ece114c2e518fb96c2
ZubkovMaxi <zubk.m@mail.ru>
2022-12-11 14:20:22 +0600

    comm

--- nearest.py
+++ nearest.py
@@ -1,4 +1,4 @@
-def nearest(array: str) -> str:
+def nearest(array: str) -> str: 
     """Функция определяет расстояние до ближайшего нуля"""
 
     array_list_int = [int(n) for n in array.split()] #из строки на входе, формируем список с интовыми значениями
@@ -6,15 +6,13 @@ def nearest(array: str) -> str:
     A_left_to_right = []
     B_right_to_left = []                             
     C_nearet_zero = []                               #резервируем память под три списка
-    
-    dist = len_array                                 #первоначальное значение дистанции до нуля обозначим максимально большое
+    dist = len_array                                 #первоначальное значение дистанции до нуля обозначим максимально возможное
     for m in array_list_int:                         #проверяем значение до 0 слева 
         if m == 0:
             dist = 0
         else:
             dist += 1
         A_left_to_right.append(dist)                 #записываем значения для кождого элемента в список
-
     dist = len_array                                 #Обновляем значение и повторяем действия с конца списка
     for a in reversed(array_list_int):
         if a == 0:
@@ -23,13 +21,10 @@ def nearest(array: str) -> str:
             dist += 1
         B_right_to_left.append(dist)
     B_right_to = reversed(B_right_to_left)
-    
     for i in range(len_array):                      #из двух списков выбираем минимальное значение соответственно индексу (с учетом что второй список записан в обратном порядке)
         b = min(A_left_to_right[i], B_right_to_left[len_array - 1 - i])
         C_nearet_zero.append(b)
-    
     return ' '.join(map(str, C_nearet_zero))
 
-            
 print(nearest('0 1 2 77 33 3 0 0 7 0'))
 
